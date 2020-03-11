60b106a9c45aeb1bd20c64f900b7508f43f7573a
BSD-Mfamilia <39650778+BSD-Mfamilia@users.noreply.github.com>
2020-03-11 12:32:46 -0400

    Fix

--- app/services/census_authorization_handler.rb
+++ app/services/census_authorization_handler.rb
@@ -68,7 +68,7 @@ class CensusAuthorizationHandler < Decidim::AuthorizationHandler
   def document_type_valid
     return nil if response.blank?
 
-    errors.add(:document_number, I18n.t("census_authorization_handler.invalid_document")) unless response.xpath("//codiRetorn").text == "KO"
+    errors.add(:document_number, I18n.t("census_authorization_handler.invalid_document")) unless response.xpath("//codiRetorn").text == "OK"
   end
 
   def response
